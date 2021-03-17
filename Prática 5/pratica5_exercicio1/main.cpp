b653dbfd1bf0bed4bc217cd113e19efe574d64ba
Júlio Alves <julio619alves@gmail.com>
2021-03-17 20:24:00 -0300

    removes set ano

--- Prática 5/pratica5_exercicio1/main.cpp
+++ Prática 5/pratica5_exercicio1/main.cpp
@@ -13,11 +13,9 @@ int main() {
     Pessoa pessoa("Julio Reis");
     Turma turma1("INF110", 2020);
     Estudante estudante("José Júlio", 102022, turma1);
+    
     std::cout << "A pessoa é: " << pessoa.get_nome() << std::endl;
     std::cout << "O estudante é: " << estudante.get_nome() << std::endl;
-    std::cout << "Nome: "<< estudante.get_nome() << " | Matrícula: " << estudante.get_matricula() << " | Turma: " << estudante.get_turma().get_codigo() << " | Ano: " << estudante.get_turma().get_ano() << std::endl;
-    
-    turma1.set_ano(2021);
     std::cout << "Nome: "<< estudante.get_nome() << " | Matrícula: " << estudante.get_matricula() << " | Turma: " << estudante.get_turma().get_codigo() << " | Ano: " << estudante.get_turma().get_ano() << std::endl;
     
     Turma turma2("INF112", 2021);
