b953db5fd9bf17add5498384da9892a2e711dc28
Júlio Alves <julio619alves@gmail.com>
2022-11-10 13:37:04 -0300

    fix work 1-a

--- INF330/Trabalho 1/A/a.cpp
+++ INF330/Trabalho 1/A/a.cpp
@@ -16,34 +16,36 @@ int main()
 
     for (int i = 0; i < t; i++)
     {
+        bool isIncidence = true;
+
         cin >> n;
         cin >> m;
-        int matriz[n][m];
-        bool isIncidence = true;
+
+        vector<vector<int>> matriz(n, vector<int>(m, 0));
 
         for (int j = 0; j < n; j++)
             for (int k = 0; k < m; k++)
                 cin >> matriz[j][k];
 
-        int arestas[n][n] = {};
+        vector<vector<int>> arestas(n, vector<int>(n, 0));
 
         for (int k = 0; k < m; k++)
         {
             int cont = 0;
-            int u, v;
+            int u = 0, v = 0;
 
             for (int j = 0; j < n; j++)
                 if (matriz[j][k])
                     cont++ == 0 ? u = j : v = j;
 
             if (cont != 2 || arestas[u][v])
+            {
                 isIncidence = false;
+                break;
+            }
 
             arestas[u][v]++;
             arestas[v][u]++;
-
-            if (!isIncidence)
-                break;
         }
 
         cout << (isIncidence ? "Yes" : "No") << endl;
