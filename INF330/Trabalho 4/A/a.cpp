b9f8596c89af5ce9a16143d2d3ae94144d82af21
jknvlvxs <julio619alves@gmail.com>
2022-12-06 19:06:23 -0300

    working

--- INF330/Trabalho 4/A/a.cpp
+++ INF330/Trabalho 4/A/a.cpp
@@ -16,7 +16,7 @@ void distances(vector<vector<int>> const &doors, vector<int> &distances, vector<
 
     vector<int> queue;
     queue.push_back(origem);
-    // visitou[origem] = true;
+    visitou[origem] = true;
 
     while (!queue.empty()) {
         int v = queue.back();
@@ -41,22 +41,28 @@ bool meetEachOther(int number_of_rooms, vector<int> &cat_d, vector<int> &rat_d) 
     return false;
 }
 
-bool canWalk(vector<int> const &cat_d, vector<int> const &rat_d, int number_of_rooms, vector<vector<int>> rat_doors) {
-    int counter = 0;
+bool canWalk(vector<vector<int>> &only_rat_doors, int number_of_rooms, int rat_room) {
+    vector<int> only_rat_distances(number_of_rooms, -1);
+    vector<bool> only_rat_visited(only_rat_doors.size(), false);
+    vector<int> queue;
 
-    cout << endl;
-    for (int i = 0; i < number_of_rooms; i++)
-        cout << cat_d[i] << " ";
-    cout << endl;
-    for (int i = 0; i < number_of_rooms; i++)
-        cout << rat_d[i] << " ";
-    cout << endl;
+    queue.push_back(rat_room);
 
-    for (int i = 0; i < number_of_rooms; i++)
-        if (cat_d[i] == -1 && rat_d[i] != -1)
-            counter++;
+    while (!queue.empty()) {
+        int v = queue.back();
+        queue.pop_back();
 
-    return counter >= 2;
+        for (int i = 0; i < only_rat_doors[v].size(); i++) {
+            int x = only_rat_doors[v][i];
+            if (!only_rat_visited[x]) {
+                only_rat_visited[x] = true;
+                only_rat_distances[x] = only_rat_distances[v] + 1;
+                queue.push_back(x);
+            }
+        }
+    }
+
+    return only_rat_distances[rat_room] != -1;
 }
 
 int main() {
@@ -73,12 +79,16 @@ int main() {
 
         vector<vector<int>> cat_doors(number_of_rooms);
         vector<vector<int>> rat_doors(number_of_rooms);
+        vector<vector<int>> only_rat_doors(number_of_rooms); // rat doors that are not visited by cat
 
         int u, v;
         int reverse = false;
 
         string c;
         cin.ignore();
+
+        vector<int> cat_distances(number_of_rooms, -1);
+        vector<bool> cat_visited(cat_doors.size(), false);
 
         while (getline(cin, c)) {
             if (c == "")
@@ -87,23 +97,23 @@ int main() {
             stringstream ss(c);
             ss >> u >> v;
 
-            if (u == -1 && v == -1)
+            if (u == -1 && v == -1) {
                 reverse = true;
-            else {
+                distances(cat_doors, cat_distances, cat_visited, cat_room - 1);
+            } else {
                 if (!reverse)
                     cat_doors[u - 1].push_back(v - 1);
-                else
+                else {
                     rat_doors[u - 1].push_back(v - 1);
+
+                    if (!cat_visited[u - 1] && !cat_visited[v - 1])
+                        only_rat_doors[u - 1].push_back(v - 1);
+                }
             }
         }
 
-        vector<int> cat_distances(number_of_rooms, -1);
         vector<int> rat_distances(number_of_rooms, -1);
-
-        vector<bool> cat_visited(cat_doors.size(), false);
         vector<bool> rat_visited(rat_doors.size(), false);
-
-        distances(cat_doors, cat_distances, cat_visited, cat_room - 1);
         distances(rat_doors, rat_distances, rat_visited, rat_room - 1);
 
         // meet each other
@@ -111,7 +121,7 @@ int main() {
         cout << (meet ? "Y" : "N") << " ";
 
         // mouse can walk without being caught
-        bool can_walk = rat_visited[rat_room - 1] && (cat_room != rat_room) && canWalk(cat_distances, rat_distances, number_of_rooms, rat_doors);
+        bool can_walk = canWalk(only_rat_doors, number_of_rooms, rat_room - 1);
         cout << (can_walk ? "Y" : "N") << endl;
 
         cout << endl;
