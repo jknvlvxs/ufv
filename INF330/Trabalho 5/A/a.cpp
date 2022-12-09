2708eda1c58230ccfb9c0f8dce630fea0676a812
jknvlvxs <julio619alves@gmail.com>
2022-12-08 21:02:53 -0300

    add inputs

--- INF330/Trabalho 5/A/a.cpp
+++ INF330/Trabalho 5/A/a.cpp
@@ -12,5 +12,21 @@ Pessoas com as quais discuti sobre este exercício:
 using namespace std;
 
 int main() {
+    int test_cases;
+    string suit1, suit2;
+
+    cin >> test_cases;
+
+    for (int nt = 0; nt < test_cases; nt++) {
+        int n, volunteers;
+
+        cin >> n >> volunteers;
+
+        for (int m = 0; m < volunteers; m++) {
+            cin >> suit1 >> suit2;
+            cout << suit1 << " " << suit2 << endl;
+        }
+    }
+
     return 0;
 }
--- INF330/Trabalho 5/B/b.cpp
+++ INF330/Trabalho 5/B/b.cpp
@@ -12,5 +12,31 @@ Pessoas com as quais discuti sobre este exercício:
 using namespace std;
 
 int main() {
+
+    int number_of_regulators, number_of_links;
+
+    vector<int> capacity(number_of_regulators);
+
+    // for (int n = 0; n < number_of_regulators; n++)
+    for (auto &b : capacity)
+        cin >> b;
+
+    cin >> number_of_links;
+
+    int i, j, c;
+
+    for (int m = 0; m < number_of_links; m++) {
+        cin >> i >> j >> c;
+    }
+
+    int b, d;
+
+    cin >> b, d;
+
+    vector<int> index(b + d);
+
+    for (auto &bd : index)
+        cin >> bd;
+
     return 0;
 }
